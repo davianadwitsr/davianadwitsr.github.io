83b7576ace57faf55466f3608528e90aab52fcd1
Daviana Dwitasari Enka Fornia <113608076+davianadwitsr@users.noreply.github.com>
2022-10-04 17:38:26 +0800

    Add files via upload

---
+++ Postest3/index.js
@@ -0,0 +1,9 @@
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+    document.body.classList.toggle("dark")
+})
+
+document.getElementById("dom1").style.color = "rgb(100,800,400)";
+document.getElementById("dom2").innerHTML = "Selamat Datang";
+
+alert("Have a good day, and welcome to officially website Marble Hair Salon")
